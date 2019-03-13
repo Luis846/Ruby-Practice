85a36500221f6c55d9fc1c10a7133659b141e086
Luis <luis.rivera846@gmail.com>
2019-03-12 21:58:39 -0400

    added beer on wall ruby practice

--- calc.rb
+++ calc.rb
@@ -1,1 +1,9 @@
-puts 365 * 24 * 60 * 60 * 28
+bottles = 100
+
+while bottles > 0
+  puts  bottles.to_s + ' bottles of beer on the wall ' + bottles.to_s 
+  bottles -= 1
+  puts ' bottles of beer, take one down pass it around ' + bottles.to_s + ' bottles of beer on the wall'
+end
+
+
