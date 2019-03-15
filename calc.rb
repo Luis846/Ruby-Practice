3903a8f88849814139ce783d58e6131524bb75e0
Luis <luis.rivera846@gmail.com>
2019-03-15 14:40:35 -0400

    added classes

--- calc.rb
+++ calc.rb
@@ -15,89 +15,106 @@
 #     puts 'No, not since 1938!'
 
 
-many_words = gets.chomp
+# many_words = gets.chomp
 
-def englishNumber number
-    if number < 0 
-      return 'Please enter a number that isn\'t negative.'
-    end
-    if number == 0
-      return 'zero'
-    end
+# def englishNumber number
+#     if number < 0 
+#       return 'Please enter a number that isn\'t negative.'
+#     end
+#     if number == 0
+#       return 'zero'
+#     end
    
-!
+# !
   
-    numString = '' 
+#     numString = '' 
   
-    onesPlace = ['one',     'two',       'three',    'four',     'five',
-                 'six',     'seven',     'eight',    'nine']
-    tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
-                 'sixty',   'seventy',   'eighty',   'ninety']
-    teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
-                 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+#     onesPlace = ['one',     'two',       'three',    'four',     'five',
+#                  'six',     'seven',     'eight',    'nine']
+#     tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+#                  'sixty',   'seventy',   'eighty',   'ninety']
+#     teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+#                  'sixteen', 'seventeen', 'eighteen', 'nineteen']
   
     
-    left  = number
-    write = left/100          
-    left  = left - write*100  
+#     left  = number
+#     write = left/100          
+#     left  = left - write*100  
   
-    if write > 0
+#     if write > 0
     
-      hundreds  = englishNumber write
-      numString = numString + hundreds + ' hundred'
+#       hundreds  = englishNumber write
+#       numString = numString + hundreds + ' hundred'
       
   
-      if left > 0
+#       if left > 0
         
-        numString = numString + ' '
-      end
-    end
+#         numString = numString + ' '
+#       end
+#     end
   
-    write = left/10          
-    left  = left - write*10  
+#     write = left/10          
+#     left  = left - write*10  
   
-    if write > 0
-      if ((write == 1) and (left > 0))
+#     if write > 0
+#       if ((write == 1) and (left > 0))
        
-        numString = numString + teenagers[left-1]
+#         numString = numString + teenagers[left-1]
         
   
         
-        left = 0
-      else
-        numString = numString + tensPlace[write-1]
+#         left = 0
+#       else
+#         numString = numString + tensPlace[write-1]
         
-      end
+#       end
   
-      if left > 0
+#       if left > 0
         
-        numString = numString + '-'
-      end
-    end
+#         numString = numString + '-'
+#       end
+#     end
   
-    write = left  
-    left  = 0     
+#     write = left  
+#     left  = 0     
   
-    if write > 0
-      numString = numString + onesPlace[write-1]
+#     if write > 0
+#       numString = numString + onesPlace[write-1]
       
-    end
+#     end
   
     
-    numString
+#     numString
+#   end
+  
+#   puts englishNumber(  0)
+#   puts englishNumber(  9)
+#   puts englishNumber( 10)
+#   puts englishNumber( 11)
+#   puts englishNumber( 17)
+#   puts englishNumber( 32)
+#   puts englishNumber( 88)
+#   puts englishNumber( 99)
+#   puts englishNumber(100)
+#   puts englishNumber(101)
+#   puts englishNumber(234)
+#   puts englishNumber(3211)
+#   puts englishNumber(999999)
+#   puts englishNumber(1000000000000)
+
+# creating classes
+class Die
+
+    def roll
+      1 + rand(6)
+    end
+  
   end
   
-  puts englishNumber(  0)
-  puts englishNumber(  9)
-  puts englishNumber( 10)
-  puts englishNumber( 11)
-  puts englishNumber( 17)
-  puts englishNumber( 32)
-  puts englishNumber( 88)
-  puts englishNumber( 99)
-  puts englishNumber(100)
-  puts englishNumber(101)
-  puts englishNumber(234)
-  puts englishNumber(3211)
-  puts englishNumber(999999)
-  puts englishNumber(1000000000000)
+  # Let's make a couple of dice...
+  dice = [Die.new, Die.new]
+  
+  # ...and roll them.
+  dice.each do |die|
+    puts die.roll
+  end
